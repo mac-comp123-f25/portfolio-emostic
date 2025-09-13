e76422bcc5193e86f5d90e563389afbe2b7d06b8
emostic <emostic@macalester.edu>
2025-09-13 11:25:04 -0500

    thank-you note in greet.py

---
+++ src/ica/02-data_types/greet.py
@@ -0,0 +1,8 @@
+kind_person = 'Aunt Judi'
+
+thank_you_note = "Dear " + kind_person + ",\n"
+thank_you_note = thank_you_note + "Thank you for the graduation present. I can't wait to use it. I"
+thank_you_note = thank_you_note + " hope you are enjoying your summer. You're the best, " + kind_person + "!\n"
+thank_you_note = thank_you_note + "Love, Mittens"
+
+print(thank_you_note)
