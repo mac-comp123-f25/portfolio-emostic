b59be0d630a56eb6584400ec90e6b1c11030fbdf
emostic <emostic@macalester.edu>
2025-09-13 11:15:22 -0500

    Update banana.py

--- src/ica/02-data_types/banana.py
+++ src/ica/02-data_types/banana.py
@@ -1,3 +1,11 @@
 #define variables
 banana_length = 15.5
 num_bananas = 5100
+
+#calculate total length in cm
+total_length_cm = banana_length * num_bananas
+
+#convert to meters (dividing by 100)
+total_length_meters = total_length_cm / 100
+
+print("Total length of", num_bananas, "bananas:", total_length_meters, "meters")
