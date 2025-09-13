7a22ccc88435db55f9eab59ef0ad7468aa8b0f6b
emostic <emostic@macalester.edu>
2025-09-12 20:04:25 -0500

    During the class!

---
+++ src/ica/03-turtle/star.py
@@ -0,0 +1,13 @@
+import turtle
+
+window = turtle.Screen()
+my_turtle = turtle.Turtle()
+
+t1 = turtle.Turtle()
+t1.color("blue")
+t1.shape("turtle")
+
+for i in range(5):
+    t1.forward(100)
+    t1.right(144)
+turtle.done()
---
+++ src/ica/03-turtle/triangle.py
@@ -0,0 +1,15 @@
+import turtle
+
+window = turtle.Screen()
+my_turtle = turtle.Turtle()
+
+window.bgcolor("#FFF8DC")
+my_turtle.color("#6495ED")
+my_turtle.forward(50)
+my_turtle.left(45)
+my_turtle.forward(50)
+my_turtle.left(155)
+my_turtle.forward(100)
+window.exitonclick() # this line closes the window when user clicks
+
+
