faab3f3c02feccea0a645572bedd6d027beae057
emostic <emostic@macalester.edu>
2025-09-13 11:28:27 -0500

    Lists in greet.py

--- src/ica/02-data_types/greet.py
+++ src/ica/02-data_types/greet.py
@@ -6,3 +6,22 @@ thank_you_note = thank_you_note + " hope you are enjoying your summer. You're th
 thank_you_note = thank_you_note + "Love, Mittens"
 
 print(thank_you_note)
+
+#list examples to test
+lst1 = [5, 1, 9, 2]
+lst2 = ['Nauru', 'Kiribati', 'Vanuatu']
+
+print("Length of lst1:", len(lst1))
+print("Length of lst2:", len(lst2))
+
+lst3 = lst1 + lst2
+print("lst3 (combined):", lst3)
+
+lst4 = ['a', 'b'] * 5
+print("lst4 (repeated):", lst4)
+
+print("'Kiribati' in lst2:", 'Kiribati' in lst2)
+print("'Fiji' in lst2:", 'Fiji' in lst2)
+print("lst1[0]:", lst1[0])
+print("lst2[2]:", lst2[2])
+print("lst1[2:3]:", lst1[2:3])
