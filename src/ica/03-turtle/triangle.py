e03c478593ed403e5cf6b43766624d60601116d8
emostic <emostic@macalester.edu>
2025-09-14 12:48:35 -0500

    Update triangle.py
    Used for loop to make a triangle.

--- src/ica/03-turtle/triangle.py
+++ src/ica/03-turtle/triangle.py
@@ -5,11 +5,15 @@ my_turtle = turtle.Turtle()
 
 window.bgcolor("#FFF8DC")
 my_turtle.color("#6495ED")
-my_turtle.forward(50)
-my_turtle.left(45)
-my_turtle.forward(50)
-my_turtle.left(155)
-my_turtle.forward(100)
-window.exitonclick() # this line closes the window when user clicks
+# my_turtle.forward(50)
+# my_turtle.left(45)
+# my_turtle.forward(50)
+# my_turtle.left(155)
+# my_turtle.forward(100)
+# window.exitonclick() # this line closes the window when user clicks
+for _ in range(3):
+    turtle.forward(100)
+    turtle.left(120)
 
+window.exitonclick()
 
