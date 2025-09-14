63e95b38cd7c3a7bfc4f4d0ff650925835152053
emostic <emostic@macalester.edu>
2025-09-14 12:51:17 -0500

    Update star.py
    Added the second turtle with goto method!

--- src/ica/03-turtle/star.py
+++ src/ica/03-turtle/star.py
@@ -10,4 +10,15 @@ t1.shape("turtle")
 for i in range(5):
     t1.forward(100)
     t1.right(144)
-turtle.done()
+
+#now second turtle
+t2 = turtle.Turtle()
+t2.shape("turtle")
+t2.color("red")
+t2.penup()
+t2.goto(120, 0)
+t2.pendown()
+for i in range(5):
+    t2.forward(100)
+    t2.right(144)
+window.exitonclick()
