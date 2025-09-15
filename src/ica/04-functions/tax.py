d74e9267bfb3cd383e17b7b5c7f70f33b06310d1
emostic <emostic@macalester.edu>
2025-09-15 10:14:21 -0500

    Finished tax.py

---
+++ src/ica/04-functions/tax.py
@@ -0,0 +1,12 @@
+def add_tax(price, tax_rate):
+    tax_amt = price * tax_rate
+    return price + tax_amt
+
+'''
+Local environment
+price 25.99
+tax_rate 0.0725
+tax_amt 1.88
+'''
+add_tax(25.99, 0.0725)
+print(add_tax(25.99, 0.0725))
