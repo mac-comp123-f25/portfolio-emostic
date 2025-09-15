a8ef34e460a9ef11395a5f95a5ff67dbcef74a02
emostic <emostic@macalester.edu>
2025-09-15 10:20:54 -0500

    Update tax.py

--- src/ica/04-functions/tax.py
+++ src/ica/04-functions/tax.py
@@ -1,5 +1,7 @@
 def add_tax(price, tax_rate):
+    print('the inputs are:', price, tax_rate)
     tax_amt = price * tax_rate
+    print('the tax amount is:', tax_amt)
     return price + tax_amt
 
 '''
@@ -8,5 +10,6 @@ price 25.99
 tax_rate 0.0725
 tax_amt 1.88
 '''
+
 add_tax(25.99, 0.0725)
 print(add_tax(25.99, 0.0725))
