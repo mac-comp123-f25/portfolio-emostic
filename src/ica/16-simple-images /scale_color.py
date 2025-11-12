52d785d1866aa508faf0c146c0c7163dfc771719
emostic <emostic@macalester.edu>
2025-11-12 17:03:35 -0600

    update

--- src/ica/16-simple-images /scale_color.py
+++ src/ica/16-simple-images /scale_color.py
@@ -1,3 +1,17 @@
+from src.ica.helpers.imageTools import *
+from src.ica.helpers.dummyWindow import *
+
+pic1 = Picture("../SampleImages/blueBird.jpg")
+
+def grayscale(pic):
+    new_pic = pic.copy()
+    for (x, y) in new_pic:
+        (r, g, b) = new_pic.getColor(x, y)
+        lumin = (r + g + b) / 3
+        new_pic.setColor(x, y, (lumin, lumin, lumin))
+
+    return new_pic
+
 def weighted_scale(pic, w1, w2, w3):
     new_pic = pic.copy()
     for (x, y) in new_pic:
@@ -7,12 +21,8 @@ def weighted_scale(pic, w1, w2, w3):
 
     return new_pic
 
-def negative (pic):
-    new_pic = pic.copy()
-    for (x, y) in new_pic:
-        (r, g, b) = new_pic.getColor(x, y)
-        neg_r = 255 -r
-        neg_g = 255 - g
-        neg_b = 255 - b
-        new_pic.setColor(x, y, (neg_r, neg_g, neg_b))
-    return new_pic
+gray_pic = grayscale(pic1)
+gray_pic.save("../SampleImages/blueBird.jpg")
+gray_pic.explore()
+
+keep_windows_open()
