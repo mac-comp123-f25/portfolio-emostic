3a8f952d8f6ae0ac8930c8d68f28b4fe0b46382d
emostic <emostic@macalester.edu>
2025-11-12 10:16:38 -0600

    negative_color done

--- src/ica/16-simple-images /scale_color.py
+++ src/ica/16-simple-images /scale_color.py
@@ -6,3 +6,13 @@ def weighted_scale(pic, w1, w2, w3):
         new_pic.setColor(x, y, (lumin, lumin, lumin))
 
     return new_pic
+
+def negative (pic):
+    new_pic = pic.copy()
+    for (x, y) in new_pic:
+        (r, g, b) = new_pic.getColor(x, y)
+        neg_r = 255 -r
+        neg_g = 255 - g
+        neg_b = 255 - b
+        new_pic.setColor(x, y, (neg_r, neg_g, neg_b))
+    return new_pic
